1e0aa1eccb220f6b0f13ec4bc64681c09fdd5558
Devmi Fernando <devthamashafernando@gmail.com>
2023-11-21 14:42:12 +0530

    first commit

---
+++ first.php
@@ -0,0 +1,17 @@
+<html>
+    
+    <head>
+            <title>Topic</title>
+    </head>
+    <body>
+        <h1>My first Document</h1>
+
+<?php
+
+
+    echo"First php doc";
+
+?>
+
+</body>
+</html>
